e2181b273ee53619a76063ba29d5504c4ffde7b5
grusha4669 <grusha4669@gmail.com>
2026-01-20 11:52:38 +0300

    20260120115238

--- scripts/build.hwloc.sh
+++ scripts/build.hwloc.sh
@@ -9,6 +9,7 @@ mkdir -p build && cd build
 git clone https://github.com/open-mpi/hwloc.git 
 cd hwloc/
 
+sh autogen.sh
 ./configure --disable-shared --enable-static --disable-io --disable-libudev --disable-libxml2
 make -j$(nproc || sysctl -n hw.ncpu || sysctl -n hw.logicalcpu)
 cp -fr include ../../deps
